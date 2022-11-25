4e90e214f78756a7ff8adb07614e824d2071a6d8
GeraldineAillon <117864297+GeraldineAillon@users.noreply.github.com>
2022-11-24 22:44:28 -0300

    Delete reservaEspacio.js

--- routes/reservaEspacio.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const reservaEspacioController = require('../controllers/reservaEspacio');
-
-const api = express.Router();
-
-api.post('/rervaEspacio',reservaEspacioController.createreservaEspacio);
-
-module.exports = api;
