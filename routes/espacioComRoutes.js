ea3292177a39600bc627b7ff562f88b0595338b3
GeraldineAillon <117864297+GeraldineAillon@users.noreply.github.com>
2022-11-24 22:44:21 -0300

    Delete espacioComRoutes.js

--- routes/espacioComRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const espacioComController = require('../controllers/espacioComController');
-
-const api = express.Router();
-
-api.post('/espacioCom',espacioComController.createEspacioCom);
-
-module.exports = api;
