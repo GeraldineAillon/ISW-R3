640d9ed1a4cf8b70931651164c2ddf88336049b9
FranciscoFariasDevs <105682325+FranciscoFariasDevs@users.noreply.github.com>
2022-11-28 02:39:57 -0300

    Update usuarioController.js

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,5 +1,4 @@
 const usuario = require('../models/usuario');
-
 const createUsuario = (req,res)=>{
     const {name,foto,nroTelefono,role} =req.body
     const newUsuario = new usuario({
@@ -15,7 +14,48 @@ const createUsuario = (req,res)=>{
         return res.status(201).send(usuario);
     })
 }
+const getusuario= (req,res)=>{
+    usuario.find({},(error,usuario)=>{
+        if(error){
+            return res.status(400).send({message:"No se pudo realizar la busqueda"})
+        }
+        if(usuario.length === 0){
+            return res.status(404).send({message:"No se encontraró el usuario"})
+        }
+        return res.status(200).send(usuario)
+    })
+}
+
+const updateusuario = (req,res)=> {
+    const {id} = req.params
+    usuario.findByIdAndUpdate(id,req.body,(error,usuario)=>{
+        if(error){
+            return res.status(400).send({message:"No se pudo actualizar los datos del usuario"})
+        }
+        if (!usuario){
+            return res.status(404).send({message:"No se encontraron usuario"})
+        }
+        return res.status(200).send({message:"Datos usuarios actualizado"})
+    })
+}
+
+const deleteusuario = (req, res) =>{
+    const {id} =req.params
+    usuario.findByIdAndDelete(id,(error,foto)=>{
+        if(error){
+            return res.status(400).send({message:"No se pudo borrar la usuario"})
+        }
+        if(!usuario){
+            return res.status(404).send({message:"No se encontró el usuario especificado"})
+        }
+        return res.status(200).send({message:"Usuario eliminado"})
+    })
+}
 
 module.exports ={
     createUsuario 
+    createUsuario, 
+    getusuario,
+    updateusuario,
+    deleteusuario
 }
