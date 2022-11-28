399086360495aef02f0d41154509cb34707c9b31
FranciscoFariasDevs <105682325+FranciscoFariasDevs@users.noreply.github.com>
2022-11-28 02:45:54 -0300

    Update usuarioRoutes.js

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -4,5 +4,9 @@ const usuarioController = require('../controllers/usuarioController.js');
 const api = express.Router();
 
 api.post('/usuario',usuarioController.createEspacioCom);
+api.post('/usuario',usuarioController.createUsuario);
+api.get('/usuario',usuarioController.getusuario);
+api.put('/usuario/update/:id', usuarioController.updateusuario);
+api.delete('/usuario/delete/:id',usuarioController.deleteusuario)
 
 module.exports = api;
