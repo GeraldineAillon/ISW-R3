cc74861d99d9cb518cc0289011a32347b4301e64
FranciscoFariasDevs <105682325+FranciscoFariasDevs@users.noreply.github.com>
2022-11-28 02:44:18 -0300

    Update sancionRoutes.js

--- routes/sancionRoutes.js
+++ routes/sancionRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const sancionController = require('../controllers/sancionController');
+const usuarioController = require('../controllers/usuarioController.js');
 
 const api = express.Router();
 
-api.post('/sancion',sancionController.createsancion);
+api.post('/usuario',usuarioController.createEspacioCom);
+api.post('/usuario',usuarioController.createUsuario);
+api.get('/usuario',usuarioController.getusuario);
+api.put('/usuario/update/:id', usuarioController.updateusuario);
+api.delete('/usuario/delete/:id',usuarioController.deleteusuario)
 
 module.exports = api;
